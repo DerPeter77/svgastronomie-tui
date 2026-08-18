88b12e8b82cd22d7742c5b2339b8f309a3f78c82
DerPeter77 <145033939+DerPeter77@users.noreply.github.com>
2026-08-18 19:42:09 +0200

    adding restaurants from tui

--- ui/pages/ChooseRestaurant.go
+++ ui/pages/ChooseRestaurant.go
@@ -7,12 +7,15 @@ import (
 	"strings"
 	"time"
 
+	"charm.land/bubbles/v2/textinput"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
 	"github.com/DerPeter77/svgastronomie-tui/ui/events"
 	"github.com/DerPeter77/svgastronomie-tui/ui/styles"
 	"github.com/goccy/go-yaml"
 )
+
+const savedDefaultPath string = "SavedRestaurants.yaml"
 
 // Types and Functions for Saving Restaurants in yaml File
 type Restaurant struct {
@@ -28,6 +31,7 @@ func getSavedRestaurantsFromYAML(path string) (SavedRestaurants, error) {
 	savedRestaurants := SavedRestaurants{}
 	file, err := os.ReadFile(path)
 	if err != nil {
+		os.Create(savedDefaultPath)
 		return savedRestaurants, err
 	}
 	yaml.Unmarshal(file, &savedRestaurants)
@@ -56,9 +60,11 @@ func addRestaurant(path string, restaurant Restaurant) error {
 	return nil
 }
 
-func AddRestaurantCmd(path string, restaurant Restaurant) tea.Cmd {
+type AddRestaurantCmdError error
+
+func AddRestaurantCmd(path string, restaurant Restaurant) tea.Msg {
 	err := addRestaurant(path, restaurant)
-	return func() tea.Msg { return err }
+	return AddRestaurantCmdError(err)
 }
 
 type RestaurantsMsg struct {
@@ -84,21 +90,44 @@ type ChooseRestaurantModel struct {
 	savedRestaurants SavedRestaurants
 	cursorRestaurant int
 	err              error
+
+	addingMode bool
+	nameInput  textinput.Model
+	urlInput   textinput.Model
 }
 
 func NewChooseRestaurantPage() ChooseRestaurantModel {
+	nameInput := textinput.New()
+	nameInput.Placeholder = "Name"
+	nameInput.SetVirtualCursor(true)
+	nameInput.Blur()
+	nameInput.CharLimit = 30
+	nameInput.SetWidth(50)
+
+	urlInput := textinput.New()
+	urlInput.Placeholder = "Url"
+	urlInput.SetVirtualCursor(true)
+	urlInput.Blur()
+	urlInput.CharLimit = 0
+	urlInput.SetWidth(50)
 	return ChooseRestaurantModel{
 		savedRestaurants: SavedRestaurants{Restaurants: []Restaurant{}},
 		cursorRestaurant: 0,
 		err:              nil,
+		addingMode:       false,
+		nameInput:        nameInput,
+		urlInput:         urlInput,
 	}
 }
 
 func (m ChooseRestaurantModel) Init() tea.Cmd {
-	return tea.Batch(ErrorTick, GetRestaurantsCmd("SavedRestaurants.yaml"))
+	return tea.Batch(ErrorTick, GetRestaurantsCmd(savedDefaultPath))
 }
 
 func (m ChooseRestaurantModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case ResetErrorTickMsg:
 		m.err = nil
@@ -106,44 +135,100 @@ func (m ChooseRestaurantModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "up":
+			if len(m.savedRestaurants.Restaurants) == 0 {
+				m.err = errors.New("Füge erstmal Restaurants hinzu!")
+				return m, nil
+			}
 			length := len(m.savedRestaurants.Restaurants)
 			m.cursorRestaurant = (m.cursorRestaurant - 1 + length) % length
-			m.err = errors.New("Test Error mit Pfeil nach oben")
 		case "down":
+			if len(m.savedRestaurants.Restaurants) == 0 {
+				m.err = errors.New("Füge erstmal Restaurants hinzu!")
+				return m, nil
+			}
 			length := len(m.savedRestaurants.Restaurants)
 			m.cursorRestaurant = (m.cursorRestaurant + 1) % length
 		case "enter":
-			events.NavigateTo(events.ShowRestaurantPageKey)
-			return m, nil
+			if m.addingMode {
+				name := m.nameInput.Value()
+				url := m.urlInput.Value()
+				m.addingMode = false
+				m.nameInput.Reset()
+				m.urlInput.Reset()
+				return m, tea.Batch(func() tea.Msg {
+					return AddRestaurantCmd(savedDefaultPath, Restaurant{Name: name, Url: url})
+				}, GetRestaurantsCmd(savedDefaultPath))
+			} else {
+				events.NavigateTo(events.ShowRestaurantPageKey)
+				return m, nil
+			}
+		case "a":
+			if !m.addingMode {
+				m.addingMode = true
+				m.nameInput.Focus()
+				m.urlInput.Blur()
+				return m, nil
+			}
+		case "tab":
+			if m.addingMode {
+				if m.nameInput.Focused() {
+					m.nameInput.Blur()
+					m.urlInput.Focus()
+				} else {
+					m.nameInput.Focus()
+					m.urlInput.Blur()
+				}
+			}
+		case "d":
+		case "r":
+			return m, GetRestaurantsCmd(savedDefaultPath)
 		case "q":
 			return m, tea.Quit
 		}
 	case RestaurantsMsg:
 		m.savedRestaurants = msg.Restaurants
 		m.err = msg.err
+	case AddRestaurantCmdError:
+		m.err = msg
 	}
+
+	m.nameInput, cmd = m.nameInput.Update(msg)
+	cmds = append(cmds, cmd)
+
+	m.urlInput, cmd = m.urlInput.Update(msg)
+	cmds = append(cmds, cmd)
+
 	return m, nil
 }
 
 func (m ChooseRestaurantModel) View() tea.View {
 	headline := styles.Headline.Render("SV Restaurant TUI")
 
-	text := styles.Text.Render("Wähle das Restaurant aus!\n")
+	text := styles.Text.Render("Wähle das Restaurant aus!")
 
-	var restaurants []string
-	for index, value := range m.savedRestaurants.Restaurants {
-		textstyle := styles.Text
-		if index == m.cursorRestaurant {
-			textstyle = styles.SelectedText
+	// Selection Mode
+	if m.addingMode == false {
+		text += styles.Text.Render("\n[R] Neu laden   -   [A] Neues Restaurant   -   [D] Ausgewähltes Restaurant löschen")
+
+		var restaurants []string
+		for index, value := range m.savedRestaurants.Restaurants {
+			textstyle := styles.Text
+			if index == m.cursorRestaurant {
+				textstyle = styles.SelectedText
+			}
+
+			restaurants = append(restaurants, textstyle.Render(fmt.Sprintf("Restaurant: %v - Url: %v", value.Name, value.Url)))
 		}
 
-		restaurants = append(restaurants, textstyle.Render(fmt.Sprintf("Restaurant: %v - Url: %v", value.Name, value.Url)))
-	}
+		text += styles.Text.Render("\n\n" + strings.Join(restaurants, "\n"))
 
-	text += styles.Text.Render("\n" + strings.Join(restaurants, "\n"))
-
-	if m.err != nil {
-		text += styles.ErrorText.Render("\n\n" + m.err.Error())
+		if m.err != nil {
+			text += styles.ErrorText.Render("\n\n" + m.err.Error())
+		}
+	} else {
+		// Adding Mode
+		text += styles.Text.Render("\n" + m.nameInput.View())
+		text += styles.Text.Render("\n" + m.urlInput.View())
 	}
 
 	finalString := lipgloss.JoinVertical(lipgloss.Left, headline, text)
