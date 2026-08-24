5eba472e9c6bc5db8bb2bcc6f0fb2a6bc38dd7ce
EchterTimo <EchterTimo@users.noreply.github.com>
2026-08-24 20:09:44 +0200

    use new version of go-svgastronomie

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 
 	tea "charm.land/bubbletea/v2"
 	"github.com/DerPeter77/svgastronomie-tui/ui"
+	sv "github.com/EchterTimo/go-svgastronomie"
 )
 
 func main() {
+	sv.EnsureInstalledAndClearTerminal()
 	p := tea.NewProgram(ui.NewAppModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Fehler beim Starten der App: %v\n", err)
